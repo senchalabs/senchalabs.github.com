112993fec3a2a4abb0034cf3e9b83dbaaa756e3b
David Kaneda <dk@morfunk.com>
2012-01-11 14:02:19 -0800

    Bringing in Recipes.

--- assets/config.rb
+++ assets/config.rb
@@ -1,4 +1,5 @@
 # Require any additional compass plugins here.
+require File.join(File.dirname(__FILE__), 'recipes', 'lib', 'compass-recipes')
 
 # Set this to the root of your project when deployed:
 http_path = "/"
