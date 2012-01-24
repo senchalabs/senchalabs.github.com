fbc005641dd36d35b402baa031f8ed0ca9b561e9
David Kaneda <dk@morfunk.com>
2012-01-24 15:12:45 -0800

    Final tweaks before tweetering.
    Added better title with keywords, revised intro copy, small grammar fixes, better Sencha link in footer, link styles for #intro. A few JS syntax fixes.

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -4,21 +4,21 @@ Ext.ns('Ext.ux');
  * Carousel
  ****************************************************/
 Ext.ux.Carousel = Ext.extend(Ext.util.Observable, {
-	// constants
-	KEY_LEFT: 37,
-	KEY_UP: 38,
-	KEY_RIGHT: 39,
-	KEY_DOWN: 40,
-	// slide options
+    // constants
+    KEY_LEFT: 37,
+    KEY_UP: 38,
+    KEY_RIGHT: 39,
+    KEY_DOWN: 40,
+    // slide options
     slideSelector: 'div.feature',
     slideMargin: -25,
-	// preview options
-	previewIsShowing: false,
-	// controls
-	controlLeftSelector: 'control-left',
-	controlRightSelector: 'control-right',
-	controlUpSelector: 'control-up',
-	controlDownSelector: 'control-down',
+    // preview options
+    previewIsShowing: false,
+    // controls
+    controlLeftSelector: 'control-left',
+    controlRightSelector: 'control-right',
+    controlUpSelector: 'control-up',
+    controlDownSelector: 'control-down',
     /**
      * Constructor
      * @param String id
@@ -35,83 +35,83 @@ Ext.ux.Carousel = Ext.extend(Ext.util.Observable, {
 
         // grab elements
         this.container = Ext.get(id);
-		this.previews = Ext.get('previews');
+        this.previews = Ext.get('previews');
         this.slides  = this.container.select(this.slideSelector, false);
-		this.controlLeft = Ext.get(this.controlLeftSelector);
-		this.controlRight = Ext.get(this.controlRightSelector);
-		this.controlUp = Ext.get(this.controlUpSelector);
-		this.controlDown = Ext.get(this.controlDownSelector);
+        this.controlLeft = Ext.get(this.controlLeftSelector);
+        this.controlRight = Ext.get(this.controlRightSelector);
+        this.controlUp = Ext.get(this.controlUpSelector);
+        this.controlDown = Ext.get(this.controlDownSelector);
 
         // set dims
         this.slideWidth  = this.slides.item(0).getWidth() + this.slideMargin;
         this.totalWidth  = (this.slides.getCount() * this.slideWidth);
         this.container.setWidth(this.totalWidth);
 
-		// set default slide
-		this.activeIndex = Math.floor(this.slides.getCount()/2);
-		
-		// init
-		this.initSlides();
-		this.registerKeyPressEvents()
+        // set default slide
+        this.activeIndex = Math.floor(this.slides.getCount()/2);
+        
+        // init
+        this.initSlides();
+        this.registerKeyPressEvents();
         this.goTo(this.activeIndex);
     },
-	/**
-	 * Initialize slides
-	 */
-	initSlides: function() {
-		this.slides.each(function(el, t, i) {
-			// set margin
+    /**
+     * Initialize slides
+     */
+    initSlides: function() {
+        this.slides.each(function(el, t, i) {
+            // set margin
             el.setStyle('marginRight', this.slideMargin + "px");
-			// get the project title
-			var title = el.select('h1').elements[0].innerText;
-			// create the preview
-			var pr = this.createPreview(el.getAttribute('data-preview'), title);
-			// set click handler
+            // get the project title
+            var title = el.select('h1').elements[0].innerText;
+            // create the preview
+            var pr = this.createPreview(el.getAttribute('data-preview'), title);
+            // set click handler
             el.on('click', function(evt, an) {
                 this.goTo(i);
             }, this);
             pr.on('click', function(evt, an) {
-				this.hidePreviews();
+                this.hidePreviews();
                 this.goTo(i);
             }, this);
-        }, this);	
-	},
-	/**
-	 * Create the project preview
-	 * @return Element
-	 */
-	createPreview: function(p,t) {
-		var preview = Ext.get(Ext.DomHelper.append(this.previews, { class: 'preview' }));
-		preview.setStyle('backgroundImage', 'url("'+p+'")');
-		Ext.DomHelper.append(preview, {
-			tag: 'h2',
-			html: t
-		});
-		return preview;
-	},
-	/**
-	 * Register key press events
-	 */
-	registerKeyPressEvents: function() {
-		Ext.EventManager.addListener(window, 'keydown', function(e,t) {
-			if(e.keyCode == this.KEY_LEFT && !this.previewIsShowing) {
-				this.onKeyLeft(e);
-			}
-			if(e.keyCode == this.KEY_RIGHT && !this.previewIsShowing) {
-				this.onKeyRight(e);
-			}
-			if(e.keyCode == this.KEY_UP) {
-				this.onKeyUp(e);
-			}
-			if(e.keyCode == this.KEY_DOWN) {
-				this.onKeyDown(e);
-			}
-		}, this);
-		this.controlLeft.on('click', this.onKeyLeft, this);
-		this.controlRight.on('click', this.onKeyRight, this);
-		this.controlUp.on('click', this.onKeyUp, this);
-		this.controlDown.on('click', this.onKeyDown, this);
-	},
+        }, this);
+    },
+    /**
+     * Create the project preview
+     * @return Element
+     */
+    createPreview: function(p,t) {
+        var preview = Ext.get(Ext.DomHelper.append(this.previews, { class: 'preview' }));
+        preview.setStyle('backgroundImage', 'url("'+p+'")');
+        Ext.DomHelper.append(preview, {
+            tag: 'h2',
+            html: t
+        });
+        return preview;
+    },
+    /**
+     * Register key press events
+     */
+    registerKeyPressEvents: function() {
+        Ext.EventManager.addListener(window, 'keydown', function(e,t) {
+            if(e.keyCode == this.KEY_LEFT && !this.previewIsShowing) {
+                this.onKeyLeft(e);
+            }
+            if(e.keyCode == this.KEY_RIGHT && !this.previewIsShowing) {
+                this.onKeyRight(e);
+            }
+            if(e.keyCode == this.KEY_UP) {
+                this.onKeyUp(e);
+            }
+            if(e.keyCode == this.KEY_DOWN) {
+                this.onKeyDown(e);
+            }
+        }, this);
+        this.controlLeft.on('click', this.onKeyLeft, this);
+        this.controlRight.on('click', this.onKeyRight, this);
+        this.controlUp.on('click', this.onKeyUp, this);
+        this.controlDown.on('click', this.onKeyDown, this);
+    },
     /**
      * Get the left position of a specific slide
      * @param int index
@@ -126,7 +126,7 @@ Ext.ux.Carousel = Ext.extend(Ext.util.Observable, {
      */
     goTo: function(index) {
         if(index < this.slides.getCount() && index >= 0) {
-			this.container.setLeft(this.getXPos(index));
+            this.container.setLeft(this.getXPos(index));
             this.activeIndex = index;
             this.onActivate();
         }
@@ -151,18 +151,18 @@ Ext.ux.Carousel = Ext.extend(Ext.util.Observable, {
         }
         this.goTo(this.activeIndex);
     },
-	showPreviews: function() {
-		this.onDeactivate();
-		this.container.removeClass('active');
-		this.previews.addClass('active');
-		this.previewIsShowing = true;
-	},
-	hidePreviews: function() {
-		this.onActivate();
-		this.container.addClass('active');
-		this.previews.removeClass('active');
-		this.previewIsShowing = false;
-	},
+    showPreviews: function() {
+        this.onDeactivate();
+        this.container.removeClass('active');
+        this.previews.addClass('active');
+        this.previewIsShowing = true;
+    },
+    hidePreviews: function() {
+        this.onActivate();
+        this.container.addClass('active');
+        this.previews.removeClass('active');
+        this.previewIsShowing = false;
+    },
     /**
      * On project activate
      */
@@ -170,82 +170,86 @@ Ext.ux.Carousel = Ext.extend(Ext.util.Observable, {
         this.slides.each(function(el, t, i) {
             if(i == this.activeIndex) {
                 el.addClass('active');
-				el.removeClass('inactive');
+                el.removeClass('inactive');
             } else {
-	            el.removeClass('active');
-				el.addClass('inactive');
-			}
+                el.removeClass('active');
+                el.addClass('inactive');
+            }
         }, this);
     },
     /**
      * On projects deactivate
      */
-	onDeactivate: function() {
-		this.slides.each(function(el, t, i) {
+    onDeactivate: function() {
+        this.slides.each(function(el, t, i) {
             el.removeClass('active');
         }, this);
-	},
-	/**
-	 * On left key event
-	 * @param Event e
-	 */
-	onKeyLeft: function(e) {
-		this.prev();
-		this.onKeyToggle(this.controlLeft);
-	},
-	/**
-	 * On right key event
-	 * @param Event e
-	 */
-	onKeyRight: function(e) {
-		this.next();
-		this.onKeyToggle(this.controlRight);
-	},
-	/**
-	 * On up key event
-	 * @param Event e
-	 */
-	onKeyUp: function(e) {
-		this.togglePreviews();			
-		this.onKeyToggle(this.controlUp);
-	},
-	/**
-	 * On down key event
-	 * @param Event e
-	 */
-	onKeyDown: function(e) {
-		this.togglePreviews();
-		this.onKeyToggle(this.controlDown);
-	},
-	/**
-	 * Toggle key classes
-	 * @param Element el
-	 */
-	onKeyToggle: function(el) {
-		el.addClass('active');
-		setTimeout(function() {
-			el.removeClass('active');
-		}, 500);
-	},
-	/**
-	 * Toggle previews
-	 */
-	togglePreviews: function() {
-		this.previewIsShowing ? this.hidePreviews() : this.showPreviews();
-	}
+    },
+    /**
+     * On left key event
+     * @param Event e
+     */
+    onKeyLeft: function(e) {
+        this.prev();
+        this.onKeyToggle(this.controlLeft);
+    },
+    /**
+     * On right key event
+     * @param Event e
+     */
+    onKeyRight: function(e) {
+        this.next();
+        this.onKeyToggle(this.controlRight);
+    },
+    /**
+     * On up key event
+     * @param Event e
+     */
+    onKeyUp: function(e) {
+        this.togglePreviews();
+        this.onKeyToggle(this.controlUp);
+    },
+    /**
+     * On down key event
+     * @param Event e
+     */
+    onKeyDown: function(e) {
+        this.togglePreviews();
+        this.onKeyToggle(this.controlDown);
+    },
+    /**
+     * Toggle key classes
+     * @param Element el
+     */
+    onKeyToggle: function(el) {
+        el.addClass('active');
+        setTimeout(function() {
+            el.removeClass('active');
+        }, 500);
+    },
+    /**
+     * Toggle previews
+     */
+    togglePreviews: function() {
+        if (this.previewIsShowing) {
+            this.hidePreviews();
+        } else {
+            this.showPreviews();
+        }
+    }
 });
 
 function toggleInfoBox(e) {
-	var intro = Ext.get('intro');
-	if(intro.hasClass('active')) {
-		intro.removeClass('active');
-	} else {
-		intro.addClass('active');
-	}
+    var intro = Ext.get('intro');
+    if(intro.hasClass('active')) {
+        intro.removeClass('active');
+    } else {
+        intro.addClass('active');
+    }
 }
 
 Ext.onReady(function() {
     var carousel = new Ext.ux.Carousel("container");
-	var about = Ext.get('about');
-	about.on('click', toggleInfoBox);
+    var about = Ext.get('about');
+    about.on('click', toggleInfoBox);
 });
